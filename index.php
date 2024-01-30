4a05db4d6a5b1db22a7b80ac7f7ae29244d4c319
unknown <bhanuchowhan652@gmail.com>
2024-01-30 16:11:17 +0530

    login complete

--- index.php
+++ index.php
@@ -1,32 +1,47 @@
 <?php
 require_once 'includes/signup_view.inc.php';
 require_once 'includes/config_session.inc.php';
+require_once 'includes/login_view.inc.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
-<h3>Login</h3>
+    <h3>Sign up</h3>
 
-<form action="includes/signup.inc.php" method="post">
-    <!-- <input type="text" name="username" placeholder="Username">
-    <input type="password" name="pwd" placeholder="Password">
-    <input type="email" name="email" placeholder="Email"> -->
+    <form action="includes/signup.inc.php" method="post">
+        <?php
+        signup_inputs();
+        ?>
+        <button>Sign Up</button>
+    </form>
+
+    <h3>Login</h3>
+
+    <form action="includes/login.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <br>
+        <input type="password" name="pwd" placeholder="Password">
+        <br>
+        <button>Login</button>
+    </form>
+
+    <form action="includes/logout.inc.php" method="post">
+        <button>Logout</button>
+    </form>
+
     <?php
-    signup_inputs();
+    check_login_errors();
     ?>
-    <button>Sign Up</button>
-</form>
 
-<?php
-check_signup_errors();
-?>
-    
 </body>
+
 </html>
